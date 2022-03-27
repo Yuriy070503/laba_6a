cd912b4b043fdb5004c6b17246519965832062d7
Yuriy070503 <Ura.rashoyan@gmail.com>
2022-03-27 20:03:51 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/laba_6a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba_6a.dir/main.cpp.obj"
+  "laba_6a.exe"
+  "laba_6a.exe.manifest"
+  "laba_6a.pdb"
+  "liblaba_6a.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laba_6a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/laba_6a.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for laba_6a.
+# This may be replaced when dependencies are built.
